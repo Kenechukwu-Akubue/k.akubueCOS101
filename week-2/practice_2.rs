909d0a26bad4a5b224c1ebdc1a0596a4a525b646
Kene Akubue <kenechukwu.akubue@pau.edu.ng>
2025-10-06 15:45:49 +0100

    Practice 2 for Week 2

---
+++ week-2/practice_2.rs
@@ -0,0 +1,6 @@
+fn main() {
+    println!(); //prints ""
+    println!("hello ");  //prints "hello"
+    println!("format {} arguments", "some"); // prints "format some arguments"
+    println!("My name is {}. I am a CS student", "Kenechukwu Jason Akubue");
+}
