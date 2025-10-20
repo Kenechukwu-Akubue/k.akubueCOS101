8dfa923b6f459b6264c5b82301d723ac2bbeaa44
Kene Akubue <kenechukwu.akubue@pau.edu.ng>
2025-10-20 15:23:47 +0100

    Week 4 Practice 8

---
+++ week-4/practice_8/target/debug/deps/practice_8.d
@@ -0,0 +1,5 @@
+C:\Users\kenej\OneDrive\Documents\School Stuff\PAU\Class Stuff\k.akubueCOS101\week-4\practice_8\target\debug\deps\practice_8.d: src\main.rs
+
+C:\Users\kenej\OneDrive\Documents\School Stuff\PAU\Class Stuff\k.akubueCOS101\week-4\practice_8\target\debug\deps\practice_8.exe: src\main.rs
+
+src\main.rs:
---
+++ week-4/practice_8/target/debug/practice_8.d
@@ -0,0 +1,1 @@
+C:\Users\kenej\OneDrive\Documents\School\ Stuff\PAU\Class\ Stuff\k.akubueCOS101\week-4\practice_8\target\debug\practice_8.exe: C:\Users\kenej\OneDrive\Documents\School\ Stuff\PAU\Class\ Stuff\k.akubueCOS101\week-4\practice_8\src\main.rs
