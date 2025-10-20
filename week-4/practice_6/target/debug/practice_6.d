bf8c8425870a4deeb4017994b077d5f10c0d056d
Kene Akubue <kenechukwu.akubue@pau.edu.ng>
2025-10-20 15:15:06 +0100

    Week 4 Practice 6

---
+++ week-4/practice_6/target/debug/practice_6.d
@@ -0,0 +1,1 @@
+C:\Users\kenej\OneDrive\Documents\School\ Stuff\PAU\Class\ Stuff\k.akubueCOS101\week-4\practice_6\target\debug\practice_6.exe: C:\Users\kenej\OneDrive\Documents\School\ Stuff\PAU\Class\ Stuff\k.akubueCOS101\week-4\practice_6\src\main.rs
