9e9acb15271e5bc670028582ee0ddbe59193ca0e
Kene Akubue <kenechukwu.akubue@pau.edu.ng>
2025-11-23 19:48:44 +0100

    Week 8 Project 1 (Updated)

--- week-8/project_1/src/main.rs
+++ week-8/project_1/src/main.rs
@@ -4,7 +4,7 @@ fn main() {
     println!("Jobs: {:?}", job);
 
     let office_admin: Vec<&str> = vec!["Intern", "Administrator", "Senior administrator", "Office Manager", "Director", "CEO"];
-    let academic: Vec<&str> = vec!["-", "Research Assitant", "PhD Candidate", "Post-Doc Researcher", "Senior Lecturer", "Dean"];
+    let academic: Vec<&str> = vec!["-", "Research Assistant", "PhD Candidate", "Post-Doc Researcher", "Senior Lecturer", "Dean"];
     let lawyer: Vec<&str> = vec!["Paralegal", "Junior Associate", "Associate", "Senior Associate 1-2","Senior Associate 3-4", "Partner"];
     let teacher: Vec<&str> = vec!["Placement", "Classroom Teacher", "Snr Teacher", "Leading Teacher", "Deputy Principal", "Principal"];
 
@@ -12,22 +12,21 @@ fn main() {
     let mut choice = String::new();
     io::stdin().read_line(&mut choice).expect("FAILED TO READ LINE");
     let choice = choice.trim().to_lowercase();
+    println!();
 
-    if choice == job[0].to_lowercase() {
+    if choice == job[0].to_lowercase().trim() {
         println!("Job titles: {:?}", office_admin);
         aps(office_admin);
-    } else if choice == job[1].to_lowercase() {
+    } else if choice == job[1].to_lowercase().trim() {
         println!("Job titles: {:?}", academic);
         aps(academic);
-    } else if choice == job[2].to_lowercase() {
+    } else if choice == job[2].to_lowercase().trim() {
         println!("Job titles: {:?}", lawyer);
         aps(lawyer);
-    } else if choice == job[3].to_lowercase() {
+    } else if choice == job[3].to_lowercase().trim() {
         println!("Job titles: {:?}", teacher);
         aps(teacher);
-    } else {println!("Invalid Job entered.")}
-    
-
+    } else {println!("Invalid Job entered.")} 
 }
 
 fn aps(x:Vec<&str>) {
@@ -35,18 +34,28 @@ fn aps(x:Vec<&str>) {
     println!("Enter job title: ");
     io::stdin().read_line(&mut lvl).expect("FAILED TO READ LINE");
     let lvl:String = lvl.trim().to_lowercase();
+    println!();
 
-    if lvl == x[0].to_lowercase() {
+    println!("Enter years of experience: ");
+    let mut y = String::new();
+    io::stdin().read_line(&mut y).expect("FAILED TO READ LINE");
+    let y: u32 = y.trim().parse().expect("INVALID INPUT");
+    println!();
+
+    if lvl == x[0].to_lowercase() && y >= 1 && y <= 2 {
         println!("Staff Level: APS 1-2");
-    } else if lvl == x[1].to_lowercase() {
+    } else if lvl == x[1].to_lowercase() && y >= 3 && y <= 5 {
         println!("Staff Level: APS 3-5");
-    } else if lvl == x[2].to_lowercase() {
-        println!("Staff Level: APS 5-8");
-    } else if lvl == x[3].to_lowercase() {
-        println!("Staff Level: EL1 8-10");
-    } else if lvl == x[4].to_lowercase() {
-        println!("Staff Level: EL2 10-13");
-    } else if lvl == x[5].to_lowercase() {
+    } else if lvl == x[2].to_lowercase() && y >= 6 && y <= 8 {
+        println!("Staff Level: APS 6-8");
+        //using 6-8 instead of 5-8 to avoid ovwerlap errors
+    } else if lvl == x[3].to_lowercase() && y >= 9 && y <= 10 {
+        println!("Staff Level: EL1 9-10");
+        //9-10 instead of 8-10 to avoid overlap errors
+    } else if lvl == x[4].to_lowercase() && y >= 11 && y <= 13 {
+        println!("Staff Level: EL2 11-13");
+        //11-13 instead of 10-13 to avoid overlap errors
+    } else if lvl == x[5].to_lowercase() && y > 13 {
         println!("Staff Level: SES");
-    } else {println!("Invalid job title entered.");}
+    } else {println!("Invalid job title entered, or years of experience do not match APS levels");}
 }
