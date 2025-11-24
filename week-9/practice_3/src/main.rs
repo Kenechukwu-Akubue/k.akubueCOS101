6f98cf508aeb0170cd6a5a6f739465ae6b7045c2
Kene Akubue <kenechukwu.akubue@pau.edu.ng>
2025-11-24 09:40:06 +0100

    Week 9 Practice 3 Updated

--- week-9/practice_3/src/main.rs
+++ week-9/practice_3/src/main.rs
@@ -1,5 +1,6 @@
 use std::fs;
 fn main() {
+    fs::File::create("data.txt").expect("CREATE FAILED");
     fs::remove_file("data.txt").expect("FAILED TO REMOVE FILE");
     println!("File removed successfully");
 }
